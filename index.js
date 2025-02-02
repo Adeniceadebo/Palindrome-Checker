6c7d5fa5555777db400dec613c17bafe28aad31d
Adebowalem Adedayo <adeniceadebo@gmail.com>
2025-02-02 21:55:06 +0100

    changes

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ function reverseString(str) {
   return str.split("").reverse("").join("");
 }
 
+function emptyString() {
+  return;
+}
+
 function check() {
-  const value = input.value;
+  const value = input.value.trim().toLowerCase(); //trim whitespace
+
+  if (value == "") {
+    alert("Type something");
+    return;
+  }
+
   const reverse = reverseString(value);
 
   if (value == reverse) {
+    alert("PALINDROME");
   } else {
     alert("Not today!");
   }
-  input.value = "";
+  input.value = ""; //clear the input field
 }
